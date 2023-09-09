cf365f3bd92da62912bc9c9dacbe0ce853baeee2
Jaehong Choe <qnalsfj@gmail.com>
2023-09-09 18:58:44 +0900

    init 프로젝트 초기화

---
+++ README.md
@@ -0,0 +1,1 @@
+# SAP BTP Cloud Foundry Runtime 배포 연습용 레파지토리
---
+++ src/test/java/btp/cf/backenddemo/BackendDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package btp.cf.backenddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
